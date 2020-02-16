c9b42d96f793e4e05dcf5135418e5254f9a72150
zdmc23 <191707+zdmc23@users.noreply.github.com>
2020-02-16 03:20:55 -0500

    Azure support

---
+++ bin/azure/cli/command_modules/cdn/_help.py
@@ -0,0 +1,325 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+# pylint: disable=line-too-long, too-many-lines
+
+helps['cdn'] = """
+type: group
+short-summary: Manage Azure Content Delivery Networks (CDNs).
+"""
+
+helps['cdn custom-domain'] = """
+type: group
+short-summary: Manage Azure CDN Custom Domains to provide custom host names for endpoints.
+"""
+
+helps['cdn custom-domain create'] = """
+type: command
+short-summary: Create a new custom domain to provide a hostname for a CDN endpoint.
+long-summary: >
+    Creates a new custom domain which must point to the hostname of the endpoint.
+    For example, the custom domain hostname cdn.contoso.com would need to have a
+    CNAME record pointing to the hostname of the endpoint related to this custom
+    domain.
+parameters:
+  - name: --profile-name
+    type: string
+    short-summary: Name of the CDN profile which is unique within the resource group.
+  - name: --endpoint-name
+    type: string
+    short-summary: Name of the endpoint under the profile which is unique globally.
+  - name: --hostname
+    type: string
+    short-summary: The host name of the custom domain. Must be a domain name.
+examples:
+  - name: Create a custom domain within an endpoint and profile.
+    text: >
+        az cdn custom-domain create -g group --endpoint-name endpoint --profile-name profile \\
+            -n domain-name --hostname www.example.com
+"""
+
+helps['cdn custom-domain delete'] = """
+type: command
+short-summary: Delete the custom domain of a CDN.
+examples:
+  - name: Delete a custom domain.
+    text: >
+        az cdn custom-domain delete -g group --endpoint-name endpoint --profile-name profile \\
+            -n domain-name
+"""
+
+helps['cdn custom-domain show'] = """
+type: command
+short-summary: Show details for the custom domain of a CDN.
+examples:
+  - name: Get the details of a custom domain.
+    text: >
+        az cdn custom-domain show -g group --endpoint-name endpoint --profile-name profile \\
+            -n domain-name
+"""
+
+helps['cdn edge-node'] = """
+type: group
+short-summary: View all available CDN edge nodes.
+"""
+
+helps['cdn endpoint'] = """
+type: group
+short-summary: Manage CDN endpoints.
+"""
+
+helps['cdn endpoint create'] = """
+type: command
+short-summary: Create a named endpoint to connect to a CDN.
+examples:
+  - name: Create an endpoint to service content for hostname over HTTP or HTTPS.
+    text: >
+        az cdn endpoint create -g group -n endpoint --profile-name profile \\
+            --origin www.example.com
+  - name: Create an endpoint with a custom domain origin with HTTP and HTTPS ports.
+    text: >
+        az cdn endpoint create -g group -n endpoint --profile-name profile \\
+            --origin www.example.com 88 4444
+  - name: Create an endpoint with a custom domain with compression and only HTTPS.
+    text: >
+        az cdn endpoint create -g group -n endpoint --profile-name profile \\
+            --origin www.example.com --no-http --enable-compression
+"""
+
+helps['cdn endpoint delete'] = """
+type: command
+short-summary: Delete a CDN endpoint.
+examples:
+  - name: Delete a CDN endpoint.
+    text: >
+        az cdn endpoint delete -g group -n endpoint --profile-name profile-name
+"""
+
+helps['cdn endpoint list'] = """
+type: command
+short-summary: List available endpoints for a CDN.
+examples:
+  - name: List all endpoints within a given CDN profile.
+    text: >
+        az cdn endpoint list -g group --profile-name profile-name
+"""
+
+helps['cdn endpoint load'] = """
+type: command
+short-summary: Pre-load content for a CDN endpoint.
+examples:
+  - name: Pre-load Javascript and CSS content for an endpoint.
+    text: >
+        az cdn endpoint load -g group -n endpoint --profile-name profile-name --content-paths \\
+            '/scripts/app.js' '/styles/main.css'
+"""
+
+helps['cdn endpoint purge'] = """
+type: command
+short-summary: Purge pre-loaded content for a CDN endpoint.
+examples:
+  - name: Purge pre-loaded Javascript and CSS content.
+    text: >
+        az cdn endpoint purge -g group -n endpoint --profile-name profile-name --content-paths \\
+            '/scripts/app.js' '/styles/*'
+"""
+
+helps['cdn endpoint start'] = """
+type: command
+short-summary: Start a CDN endpoint.
+examples:
+  - name: Start a CDN endpoint.
+    text: >
+        az cdn endpoint start -g group -n endpoint --profile-name profile-name
+"""
+
+helps['cdn endpoint stop'] = """
+type: command
+short-summary: Stop a CDN endpoint.
+examples:
+  - name: Stop a CDN endpoint.
+    text: >
+        az cdn endpoint stop -g group -n endpoint --profile-name profile-name
+"""
+
+helps['cdn endpoint update'] = """
+type: command
+short-summary: Update a CDN endpoint to manage how content is delivered.
+examples:
+  - name: Turn off HTTP traffic for an endpoint.
+    text: >
+        az cdn endpoint update -g group -n endpoint --profile-name profile --no-http
+  - name: Enable content compression for an endpoint.
+    text: >
+        az cdn endpoint update -g group -n endpoint --profile-name profile \\
+            --enable-compression
+"""
+
+helps['cdn endpoint rule'] = """
+type: group
+short-summary: Manage delivery rules for an endpoint.
+"""
+
+helps['cdn endpoint rule add'] = """
+type: command
+short-summary: Add a delivery rule to a CDN endpoint.
+examples:
+  - name: Create a global rule to disable caching.
+    text: >
+        az cdn endpoint rule add -g group -n endpoint --profile-name profile --order 0\\
+            --rule-name global --action-name CacheExpiration --cache-behavior BypassCache
+  - name: Create a rule for http to https redirect
+    text: >
+        az cdn endpoint rule add -g group -n endpoint --profile-name profile --order 1\\
+            --rule-name "redirect" --match-variable RequestScheme --operator Equal --match-values HTTPS\\
+            --action-name "UrlRedirect" --redirect-protocol Https --redirect-type Moved
+"""
+
+helps['cdn endpoint rule remove'] = """
+type: command
+short-summary: Remove a delivery rule from an endpoint.
+examples:
+  - name: Remove the global rule.
+    text: >
+        az cdn endpoint rule remove -g group -n endpoint --profile-name profile --rule-name Global\\
+"""
+
+helps['cdn endpoint rule show'] = """
+type: command
+short-summary: Show delivery rules asscociate with the endpoint.
+examples:
+  - name: show delivery rules asscociate with the endpoint.
+    text: >
+        az cdn endpoint rule show -g group --profile-name profile-name
+"""
+
+helps['cdn endpoint rule condition'] = """
+type: group
+short-summary: Manage delivery rule conditions for an endpoint.
+"""
+
+helps['cdn endpoint rule condition add'] = """
+type: command
+short-summary: Add a condition to a delivery rule.
+examples:
+  - name: Add a remote address condition.
+    text: >
+        az cdn endpoint rule condition add -g group -n endpoint --profile-name profile --rule-name name\\
+            --match-variable RemoteAddress --operator GeoMatch --match-values "TH"
+"""
+
+helps['cdn endpoint rule condition remove'] = """
+type: command
+short-summary: Remove a condition from a delivery rule.
+examples:
+  - name: Remove the first condition.
+    text: >
+        az cdn endpoint rule condition remove -g group -n endpoint --profile-name profile --rule-name name\\
+            --index 0
+"""
+
+helps['cdn endpoint rule condition show'] = """
+type: command
+short-summary: show delivery rules asscociate with the endpoint.
+examples:
+  - name: show delivery rules asscociate with the endpoint.
+    text: >
+        az cdn endpoint rule condition show -g group --profile-name profile-name
+"""
+
+helps['cdn endpoint rule action'] = """
+type: group
+short-summary: Manage delivery rule actions for an endpoint.
+"""
+
+helps['cdn endpoint rule action add'] = """
+type: command
+short-summary: Add an action to a delivery rule.
+examples:
+  - name: Add a redirect action.
+    text: >
+        az cdn endpoint rule action add -g group -n endpoint --profile-name profile --rule-name name\\
+            --action-name "UrlRedirect" --redirect-protocol HTTPS --redirect-type Moved
+  - name: Add a cache expiration action
+    text: >
+        az cdn endpoint rule action add -g group -n endpoint --profile-name profile --rule-name name\\
+            --action-name "CacheExpiration" --cache-behavior BypassCache
+"""
+
+helps['cdn endpoint rule action remove'] = """
+type: command
+short-summary: Remove an action from a delivery rule.
+examples:
+  - name: Remove the first action.
+    text: >
+        az cdn endpoint rule action remove -g group -n endpoint --profile-name profile --rule-name name\\
+            --index 0
+"""
+
+helps['cdn endpoint rule action show'] = """
+type: command
+short-summary: show delivery rules asscociate with the endpoint.
+examples:
+  - name: show delivery rules asscociate with the endpoint.
+    text: >
+        az cdn endpoint rule action show -g group --profile-name profile-name
+"""
+
+helps['cdn origin'] = """
+type: group
+short-summary: List or show existing origins related to CDN endpoints.
+"""
+
+helps['cdn profile'] = """
+type: group
+short-summary: Manage CDN profiles to define an edge network.
+"""
+
+helps['cdn profile create'] = """
+type: command
+short-summary: Create a new CDN profile.
+parameters:
+  - name: --sku
+    type: string
+    short-summary: >
+        The pricing tier (defines a CDN provider, feature list and rate) of the CDN profile.
+        Defaults to Standard_Akamai.
+examples:
+  - name: Create a CDN profile using Verizon premium CDN.
+    text: >
+        az cdn profile create -g group -n profile --sku Premium_Verizon
+  - name: Create a new CDN profile. (autogenerated)
+    text: az cdn profile create --location westus2 --name profile --resource-group group --sku Standard_Verizon
+    crafted: true
+"""
+
+helps['cdn profile delete'] = """
+type: command
+short-summary: Delete a CDN profile.
+examples:
+  - name: Delete a CDN profile.
+    text: >
+        az cdn profile delete -g group -n profile
+"""
+
+helps['cdn profile list'] = """
+type: command
+short-summary: List CDN profiles.
+examples:
+  - name: List CDN profiles in a resource group.
+    text: >
+        az cdn profile list -g group
+"""
+
+helps['cdn profile update'] = """
+type: command
+short-summary: Update a CDN profile.
+examples:
+  - name: Update a CDN profile. (autogenerated)
+    text: az cdn profile update --name MyCDNProfileWhichIsUniqueWithinResourceGroup --resource-group MyResourceGroup
+    crafted: true
+"""
---
+++ bin/azure/cli/command_modules/cdn/_params.py
@@ -0,0 +1,156 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+import argparse
+
+from knack.arguments import CLIArgumentType
+
+from azure.mgmt.cdn.models import QueryStringCachingBehavior, SkuName
+
+from azure.cli.core.commands.parameters import get_three_state_flag, tags_type, get_enum_type
+from azure.cli.core.commands.validators import get_default_location_from_resource_group
+
+from ._validators import validate_origin
+
+
+# pylint:disable=protected-access
+# pylint:disable=too-few-public-methods
+class OriginType(argparse._AppendAction):
+    def __call__(self, parser, namespace, values, option_string=None):
+        deep_created_origin = self.get_origin(values, option_string)
+        super(OriginType, self).__call__(parser, namespace, deep_created_origin, option_string)
+
+    def get_origin(self, values, option_string):
+        from azure.mgmt.cdn.models import DeepCreatedOrigin
+
+        if not 1 <= len(values) <= 3:
+            msg = '%s takes 1, 2 or 3 values, %d given'
+            raise argparse.ArgumentError(self, msg % (option_string, len(values)))
+
+        deep_created_origin = DeepCreatedOrigin(name='origin', host_name=values[0], http_port=80, https_port=443)
+        if len(values) > 1:
+            deep_created_origin.http_port = int(values[1])
+        if len(values) > 2:
+            deep_created_origin.https_port = int(values[2])
+        return deep_created_origin
+
+
+# pylint:disable=too-many-statements
+def load_arguments(self, _):
+
+    name_arg_type = CLIArgumentType(options_list=('--name', '-n'), metavar='NAME')
+    profile_name_help = 'Name of the CDN profile which is unique within the resource group.'
+
+    with self.argument_context('cdn') as c:
+        c.argument('name', name_arg_type, id_part='name')
+        c.argument('tags', tags_type)
+
+    # Profile #
+    with self.argument_context('cdn profile') as c:
+        c.argument('profile_name', name_arg_type, id_part='name', help=profile_name_help)
+
+    with self.argument_context('cdn profile create') as c:
+        c.argument('sku', arg_type=get_enum_type([item.value for item in list(SkuName)]))
+        c.argument('location', validator=get_default_location_from_resource_group)
+        c.argument('name', name_arg_type, id_part='name', help=profile_name_help)
+
+    # Endpoint #
+
+    with self.argument_context('cdn endpoint') as c:
+        c.argument('content_paths', nargs='+')
+        c.argument('endpoint_name', name_arg_type, id_part='child_name_1', help='Name of the CDN endpoint.')
+        c.argument('location', validator=get_default_location_from_resource_group)
+        c.argument('origins', options_list='--origin', nargs='+', action=OriginType, validator=validate_origin,
+                   help='Endpoint origin specified by the following space-delimited 3 '
+                        'tuple: `www.example.com http_port https_port`. The HTTP and HTTPs'
+                        'ports are optional and will default to 80 and 443 respectively.')
+        c.argument('is_http_allowed', arg_type=get_three_state_flag(invert=True), options_list='--no-http',
+                   help='Indicates whether HTTP traffic is not allowed on the endpoint. '
+                   'Default is to allow HTTP traffic.')
+        c.argument('is_https_allowed', arg_type=get_three_state_flag(invert=True), options_list='--no-https',
+                   help='Indicates whether HTTPS traffic is not allowed on the endpoint. '
+                   'Default is to allow HTTPS traffic.')
+        c.argument('is_compression_enabled', arg_type=get_three_state_flag(), options_list='--enable-compression',
+                   help='If compression is enabled, content will be served as compressed if '
+                        'user requests for a compressed version. Content won\'t be compressed '
+                        'on CDN when requested content is smaller than 1 byte or larger than 1 '
+                        'MB.')
+
+        caching_behavior = [item.value for item in list(QueryStringCachingBehavior)]
+        c.argument('query_string_caching_behavior', options_list='--query-string-caching',
+                   arg_type=get_enum_type(caching_behavior))
+        c.argument('content_types_to_compress', nargs='+')
+        c.argument('profile_name', help=profile_name_help, id_part='name')
+
+    with self.argument_context('cdn endpoint rule') as c:
+        c.argument('rule_name', help='Name of the rule.')
+        c.argument('order', help='The order of the rule. The order number must start from 0 and consecutive.\
+                    Rule with higher order will be applied later.')
+        c.argument('match_variable', arg_group="Match Condition", help='Name of the match condition.')
+        c.argument('operator', arg_group="Match Condition", help='Operator of the match condition.')
+        c.argument('selector', arg_group="Match Condition", help='Selector of the match condition.')
+        c.argument('match_values', arg_group="Match Condition", nargs='+',
+                   help='Match values of the match condition (comma separated).')
+        c.argument('transform', arg_group="Match Condition", arg_type=get_enum_type(['Lowercase', 'Uppercase']),
+                   nargs='+', help='Transform to apply before matching.')
+        c.argument('negate_condition', arg_group="Match Condition", arg_type=get_three_state_flag(),
+                   help='If true, negates the condition')
+        c.argument('action_name', arg_group="Action", help='Name of the action.')
+        c.argument('cache_behavior', arg_group="Action",
+                   arg_type=get_enum_type(['BypassCache', 'Override', 'SetIfMissing']),
+                   help='Caching behavior for the requests.')
+        c.argument('cache_duration', arg_group="Action",
+                   help='The duration for which the content needs to be cached. \
+                   Allowed format is [d.]hh:mm:ss.')
+        c.argument('header_action', arg_group="Action",
+                   arg_type=get_enum_type(['Append', 'Overwrite', 'Delete']),
+                   help='Header action for the requests.')
+        c.argument('header_name', arg_group="Action", help='Name of the header to modify.')
+        c.argument('header_value', arg_group="Action", help='Value of the header.')
+        c.argument('redirect_type', arg_group="Action",
+                   arg_type=get_enum_type(['Moved', 'Found', 'TemporaryRedirect', 'PermanentRedirect']),
+                   help='The redirect type the rule will use when redirecting traffic.')
+        c.argument('redirect_protocol', arg_group="Action",
+                   arg_type=get_enum_type(['MatchRequest', 'Http', 'Https']),
+                   help='Protocol to use for the redirect.')
+        c.argument('custom_hostname', arg_group="Action", help='Host to redirect. \
+                   Leave empty to use the incoming host as the destination host.')
+        c.argument('custom_path', arg_group="Action",
+                   help='The full path to redirect. Path cannot be empty and must start with /. \
+                   Leave empty to use the incoming path as destination path.')
+        c.argument('custom_querystring', arg_group="Action",
+                   help='The set of query strings to be placed in the redirect URL. \
+                   leave empty to preserve the incoming query string.')
+        c.argument('custom_fragment', arg_group="Action", help='Fragment to add to the redirect URL.')
+        c.argument('query_string_behavior', arg_group="Action",
+                   arg_type=get_enum_type(['Include', 'IncludeAll', 'Exclude', 'ExcludeAll']),
+                   help='Query string behavior for the requests.')
+        c.argument('query_parameters', arg_group="Action",
+                   help='Query parameters to include or exclude (comma separated).')
+        c.argument('source_pattern', arg_group="Action",
+                   help='A request URI pattern that identifies the type of requests that may be rewritten.')
+        c.argument('destination', help='The destination path to be used in the rewrite.')
+        c.argument('preserve_unmatched_path', arg_group="Action",
+                   arg_type=get_three_state_flag(),
+                   help='If True, the remaining path after the source \
+                   pattern will be appended to the new destination path.')
+        c.argument('index', type=int, help='The index of the condition/action')
+
+    with self.argument_context('cdn endpoint create') as c:
+        c.argument('name', name_arg_type, id_part='name', help='Name of the CDN endpoint.')
+
+    with self.argument_context('cdn endpoint list') as c:
+        c.argument('profile_name', id_part=None)
+
+    # Custom Domain #
+
+    with self.argument_context('cdn custom-domain') as c:
+        c.argument('custom_domain_name', name_arg_type, id_part=None, help='Name of the custom domain.')
+
+    with self.argument_context('cdn custom-domain create') as c:
+        c.argument('location', validator=get_default_location_from_resource_group)
+
+    # Origin #
+    with self.argument_context('cdn origin') as c:
+        c.argument('origin_name', name_arg_type, id_part='name')
